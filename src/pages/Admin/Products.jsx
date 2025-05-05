192e831e42c30815d6be871dc3241e3fd8f1ccd1
Govind <thisisgovindsahu@gmail.com>
2025-05-05 13:31:02 +0530

    add update feature

--- src/pages/Admin/Products.jsx
+++ src/pages/Admin/Products.jsx
@@ -6,6 +6,7 @@ const Products = () => {
   const user = JSON.parse(localStorage.getItem("user") || "{}");
   const [category, setCategory] = React.useState("");
   const [productName, setProductName] = React.useState("");
+  const [productId, setProductId] = React.useState("");
   const [priceType, setPriceType] = React.useState("single");
   const [price, setPrice] = useState("");
   const [halfPrice, setHalfPrice] = useState("");
@@ -13,8 +14,12 @@ const Products = () => {
   const [image, setImage] = useState("");
   const [imagePreview, setImagePreview] = useState(null);
   const imageRef = useRef();
+  const refImage = useRef();
   const formRef = useRef();
+  const validateCategoryRef = useRef();
   const formData = new FormData(formRef.current);
+
+  const [updateMode, setUpdateMode] = useState(false);
 
   const [products, setProducts] = useState(
     JSON.parse(localStorage.getItem("products") || "[]")
@@ -29,7 +34,6 @@ const Products = () => {
       );
       if (data?.success) {
         setProducts(data.data);
-        console.log(data);
       }
     } catch (error) {
       console.log(error);
@@ -49,7 +53,14 @@ const Products = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    if (!category) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+      validateCategoryRef.current.style.display = "block";
+      return;
+    }
     try {
       const { data } = await axios.post(
         `${
@@ -72,23 +83,79 @@ const Products = () => {
         setImage("");
         setImagePreview(null);
         formRef.current.reset();
+        fetchProducts();
       }
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleUpdateProduct = async (p) => {
+    setUpdateMode(true);
+    setProductName(p.name);
+    setCategory(p.category);
+    setPriceType(p.price_type);
+    setPrice(p.full_price);
+    setHalfPrice(p.half_price);
+    setFullPrice(p.full_price);
+    setProductId(p.id);
+    // setImage(p.image);
+    setImagePreview(
+      `${import.meta.env.VITE_API_URI}/uploaded_files/${p.image}`
+    );
+  };
+
+  const handleUpdateRequest = async (p) => {
+    if (!category) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+      validateCategoryRef.current.style.display = "block";
+      return;
+    }
+    try {
+      const { data } = await axios.post(
+        `${
+          import.meta.env.VITE_API_URI
+        }/components/routes/products/update.php?productid=${productId}&aid=${
+          user?.id
+        }`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      if (data?.success) {
+        setProductName("");
+        setCategory("");
+        setPriceType("single");
+        setPrice("");
+        setHalfPrice("");
+        setFullPrice("");
+        setImage("");
+        setImagePreview(null);
+        formRef.current.reset();
+        fetchProducts();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div id="products-page">
       <div className="form-container">
-        <form
-          ref={formRef}
-          id="add-product-form"
-          className="flex flex-col gap-10">
-          <h1 className="text-[30px] font-bold">Add Product</h1>
-          <div className="input-container flex flex-col gap-5">
+        <form ref={formRef} id="add-product-form" className="flex flex-col">
+          <h1 className="text-[30px] font-bold">
+            {updateMode ? "Update" : "Add"} Product
+          </h1>
+          <div className="input-container flex flex-col gap-2">
             <label htmlFor="product-name">Product Name</label>
             <input
+              required
               value={productName}
               type="text"
               name="name"
@@ -97,15 +164,25 @@ const Products = () => {
               onChange={(e) => setProductName(e.target.value)}
             />
           </div>
-          <div className="input-container flex flex-col gap-5">
+          <div className="input-container flex flex-col gap-2">
             <label>Select Category</label>
+            <span
+              ref={validateCategoryRef}
+              className="text-[red]"
+              style={{ display: "none" }}>
+              category name required
+            </span>
             <select
+              required
               name="category"
               id="select-input"
               value={category}
-              onChange={(e) => setCategory(e.target.value)}
+              onChange={(e) => {
+                setCategory(e.target.value);
+                validateCategoryRef.current.style.display = "none";
+              }}
               className="bg-[#222] text-white outline">
-              <option defaultChecked hidden value="">
+              <option hidden value="">
                 Select Category
               </option>
               {categories?.map((category) => (
@@ -115,9 +192,10 @@ const Products = () => {
               ))}
             </select>
           </div>
-          <div className="input-container flex flex-col gap-5">
+          <div className="input-container flex flex-col gap-2">
             <label>Price Type</label>
             <select
+              required
               value={priceType}
               name="price_type"
               id="select-input"
@@ -127,11 +205,12 @@ const Products = () => {
               <option value="both">both</option>
             </select>
           </div>
-          <div className="input-container flex flex-col gap-5">
+          <div className="input-container flex flex-col gap-2">
             {priceType === "single" ? (
               <>
                 <label htmlFor="product-price">Product Price</label>
                 <input
+                  required
                   name="full_price"
                   value={price}
                   type="number"
@@ -144,6 +223,7 @@ const Products = () => {
               <>
                 <label htmlFor="product-price">Half Price</label>
                 <input
+                  required
                   name="half_price"
                   value={halfPrice}
                   type="number"
@@ -153,6 +233,7 @@ const Products = () => {
                 />
                 <label htmlFor="product-price">Full Price</label>
                 <input
+                  required
                   name="full_price"
                   value={fullPrice}
                   type="number"
@@ -163,19 +244,36 @@ const Products = () => {
               </>
             )}
           </div>
-          <div className="input-container flex flex-col gap-5">
-            <label htmlFor="product-image">Product Image</label>
-            <input
-              name="image"
-              type="file"
-              id="product-image"
-              value={image}
-              accept=".jpg, .png, .jpeg"
-              onChange={(e) => {
-                setImage(e.target.value);
-                handleImagePreview(e);
+          <div className="input-container flex flex-col gap-2">
+            <div
+              className="input-container flex flex-col gap-2"
+              style={{ display: updateMode ? "none" : "flex" }}>
+              <label htmlFor="product-image">Product Image</label>
+              <input
+                required
+                ref={refImage}
+                name="image"
+                type="file"
+                id="product-image"
+                value={image}
+                accept=".jpg, .png, .jpeg"
+                onChange={(e) => {
+                  setImage(e.target.value);
+                  handleImagePreview(e);
+                }}
+              />
+            </div>
+            <span
+              style={{
+                display: updateMode ? "block" : "none",
               }}
-            />
+              className="upt-img"
+              onClick={(e) => {
+                setUpdateMode(false);
+                refImage.current.click();
+              }}>
+              Change Image
+            </span>
 
             <img
               ref={imageRef}
@@ -189,10 +287,12 @@ const Products = () => {
           </div>
           <button
             onClick={(e) => {
-              handleSubmit(e);
+              e.preventDefault();
+              updateMode ? handleUpdateRequest(productId) : handleSubmit(e);
+              setUpdateMode(false);
             }}
-            type="submit">
-            Add Product
+            className="w-[50%] lg:w-[20%] ">
+            {updateMode ? "Update" : "Add"} Product
           </button>
         </form>
       </div>
@@ -217,10 +317,28 @@ const Products = () => {
                     <span>Price: {p.full_price} </span>
                   ) : (
                     <span>
-                      Half Price: {p.half_price} , Full Price: {p.full_price}
+                      Half Price: {p.half_price} <br /> Full Price:{" "}
+                      {p.full_price}
                     </span>
                   )}
                 </p>
+              </div>
+              <div className="product-actions flex gap-5 justify-center items-center">
+                <button className="bg-red-500 text-white px-4 py-2 rounded">
+                  Delete
+                </button>
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded"
+                  onClick={(e) => {
+                    handleUpdateProduct(p);
+
+                    window.scrollTo({
+                      top: 0,
+                      behavior: "smooth",
+                    });
+                  }}>
+                  Update
+                </button>
               </div>
             </div>
           ))
